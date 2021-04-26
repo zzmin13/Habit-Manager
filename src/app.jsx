5a5e9865c4d05928a3ad257deab61deaa946574e
zzmin13 <zzzz1997@naver.com>
2021-04-26 17:36:47 +0900

    HTML VS JSX

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,18 @@
+import React from "react";
 import './app.css';
 
 function App() {
+  const name = undefined;
   return (
-    <h1>Hello :)</h1>
+    <>
+      <h1>Hi.</h1>
+      {
+        name && <h1>Hello {name}:)</h1>
+      }
+      {
+        ['🍇','🍋'].map((element) => <h1>{element}</h1>)
+      }
+    </>
   );
 }
 
